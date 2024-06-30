527f6f4159c9c6cd73fb1e1748c146962f25763a
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-06-30 23:34:45 +0530

    Header

--- blog_mega_project/src/components/Header/Header.jsx
+++ blog_mega_project/src/components/Header/Header.jsx
@@ -1,14 +1,73 @@
-import React from 'react'
-import LogoutBtn from './LogoutBtn'
-imp
-
+import React, { act } from "react";
+import LogoutBtn from "./LogoutBtn";
+import {Logo, Container }  from "../index";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.authStatus);
 
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
 
   return (
-    <div>Header </div>
-  )
+    <header className="py-3 shadow bg-black">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="text-white hover:bg-violet-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
