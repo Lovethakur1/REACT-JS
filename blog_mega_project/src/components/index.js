b3336fc9f533a69ae3d8c9eb3ce276b130522fff
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-06-30 23:34:49 +0530

    Update index.js

--- blog_mega_project/src/components/index.js
+++ blog_mega_project/src/components/index.js
@@ -1,7 +1,9 @@
+import Container from "./Container/Container"
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Logo from "./Logo";
 
 
 
-export { Header,Footer,Logo };
+
+export { Header,Footer,Logo , Container};
