4bcf718f1d1516948c4b5faffae94d77c7f509c8
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-06-30 23:42:31 +0530

    error fix

--- blog_mega_project/src/components/Header/Header.jsx
+++ blog_mega_project/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { act } from "react";
+import React from "react";
 import LogoutBtn from "./LogoutBtn";
 import {Logo, Container }  from "../index";
 import { Link, useNavigate } from "react-router-dom";
