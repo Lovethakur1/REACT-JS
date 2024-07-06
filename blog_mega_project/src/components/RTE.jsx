2dc5d957ffd8916682542c12630acb765892617a
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-07-06 23:35:24 +0530

    Create RTE.jsx

---
+++ blog_mega_project/src/components/RTE.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import { Controller } from 'react-hook-form'
+
+export default function RTE({name, control, label,defaultVal=''}) {
+  return (
+    <div></div>
+  )
+}
