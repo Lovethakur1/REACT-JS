768ba7195f0f5fd01c2dcc112f87e8142f78b579
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-07-08 23:41:33 +0530

    Update index.js

--- blog_mega_project/src/components/index.js
+++ blog_mega_project/src/components/index.js
@@ -1,13 +1,29 @@
-import Container from "./Container/Container"
+import Container from "./Container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Logo from "./Logo";
-import Button from './Button'
-import Input  from "./Input";
+import Button from "./Button";
+import Input from "./Input";
 import RTE from "./RTE";
 import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import Protected from "./AuthLayout";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
 
-
-
-
-export { Header,Footer,Logo , Container ,Button,Input,RTE, Select };
+export { 
+    Header,
+     Footer,
+      Logo,
+       Container,
+        Button, 
+        Input,
+         RTE,
+          Select ,
+              Signup,
+                Login,
+                 Protected,
+                  PostCard,
+                PostForm
+        };
