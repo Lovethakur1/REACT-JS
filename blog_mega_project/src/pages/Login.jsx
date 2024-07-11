12bc1559aa2186aca23fcd9dc727098c3f4d2fca
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-07-11 23:02:27 +0530

    Update Login.jsx

--- blog_mega_project/src/pages/Login.jsx
+++ blog_mega_project/src/pages/Login.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Login } from '../components'
+import { Login as LoginCom } from '../components'
 
 function Login() {
   return (
     <div className='py-8' >
-        <Login/>
+        <LoginCom/>
     </div>
   )
 }
