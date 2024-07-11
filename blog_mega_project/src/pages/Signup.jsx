d6de33cda00b00ce634fb6c958dabd754ee7998c
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-07-11 23:02:44 +0530

    Update Signup.jsx

--- blog_mega_project/src/pages/Signup.jsx
+++ blog_mega_project/src/pages/Signup.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Signup } from '../components'
+import { Signup as SingupCom } from '../components'
 
 function Signup() {
   return (
     <div className='py-8' >
-        <Signup/>
+        <SingupCom/>
     </div>
   )
 }
