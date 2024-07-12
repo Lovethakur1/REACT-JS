0e796ad6f2c33bee80163996dc67c36091b3b0bf
love thakur <113324617+Lovethakur1@users.noreply.github.com>
2024-07-13 01:33:39 +0530

    Update LogoutBtn.jsx

--- blog_mega_project/src/components/Header/LogoutBtn.jsx
+++ blog_mega_project/src/components/Header/LogoutBtn.jsx
@@ -1,34 +1,25 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import authService from "../../appwrite/auth";
-import { logout } from "../../store/authSlice";
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authService from '../../appwrite/auth'
+import {logout} from '../../store/authSlice'
 
 function LogoutBtn() {
-  const dispatch = useDispatch();
-  const logoutHandler = () => {
-    authService.logout().then(() => {
-      dispatch(logout());
-    });
-  };
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
   return (
-    <button>
-      <div class="group relative flex justify-center items-center text-zinc-600 text-sm font-bold">
-        
-
-        <div class="shadow-md flex items-center group-hover:gap-2 bg-gradient-to-r from-blue-500 to-red-500 p-3 rounded-full cursor-pointer duration-300">
-        <svg viewBox="0 0 512 512" className="w-4">
-          <path
-            fill="white"
-            d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"
-          ></path>
-        </svg>
-          <span class="text-[0px] text-gray-200 text-xl font-semibold group-hover:text-sm duration-300">
-            Logout
-          </span>
-        </div>
-      </div>
+    <button
+      className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      onClick={logoutHandler}
+    >
+      <span className="relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        LOGOUT
+      </span>
     </button>
-  );
+  )
 }
 
-export default LogoutBtn;
+export default LogoutBtn
